17bd3733377401984faabb11670f7e379effe5b7
wangxc <476387578@qq.com>
2020-03-26 03:45:28 +0800

    2

--- src/main/java/mqtt/lmq/example/demo/MQ4IoTConsumerDemo.java
+++ src/main/java/mqtt/lmq/example/demo/MQ4IoTConsumerDemo.java
@@ -24,29 +24,29 @@ public class MQ4IoTConsumerDemo {
         /**
          * MQ4IOT 实例 ID，购买后控制台获取
          */
-        String instanceId = "XXXXX";
+        String instanceId = "post-cn-mp91ky6mg06";
         /**
          * 接入点地址，购买 MQ4IOT 实例，且配置完成后即可获取，接入点地址必须填写分配的域名，不得使用 IP 地址直接连接，否则可能会导致客户端异常。
          */
-        String endPoint = "XXXXX.mqtt.aliyuncs.com";
+        String endPoint = "post-cn-mp91ky6mg06.mqtt.aliyuncs.com";
         /**
          * 账号 accesskey，从账号系统控制台获取
          */
-        String accessKey = "XXXXX";
+        String accessKey = "LTAI4FmZafkwEUFWX1JtER3o";
         /**
          * 账号 secretKey，从账号系统控制台获取，仅在Signature鉴权模式下需要设置
          */
-        String secretKey = "XXXXX";
+        String secretKey = "BdEqwy3S7tXGwv6AoGeGF4KmofgqfY";
         /**
          * MQ4IOT clientId，由业务系统分配，需要保证每个 tcp 连接都不一样，保证全局唯一，如果不同的客户端对象（tcp 连接）使用了相同的 clientId 会导致连接异常断开。
          * clientId 由两部分组成，格式为 GroupID@@@DeviceId，其中 groupId 在 MQ4IOT 控制台申请，DeviceId 由业务方自己设置，clientId 总长度不得超过64个字符。
          */
-        String clientId = "GID_XXXXX@@@XXXXX";
+        String clientId = "GID_00001@@@00004";
         /**
          * MQ4IOT 消息的一级 topic，需要在控制台申请才能使用。
          * 如果使用了没有申请或者没有被授权的 topic 会导致鉴权失败，服务端会断开客户端连接。
          */
-        final String parentTopic = "XXXXX";
+        final String parentTopic = "hscs";
         /**
          * MQ4IOT支持子级 topic，用来做自定义的过滤，此处为示意，可以填写任何字符串，具体参考https://help.aliyun.com/document_detail/42420.html?spm=a2c4g.11186623.6.544.1ea529cfAO5zV3
          * 需要注意的是，完整的 topic 长度不得超过128个字符。
